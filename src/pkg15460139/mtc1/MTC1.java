7b3076c5364c51d8eef9819f794ee49720407f52
Francisco8NGY <franciscoIsco8@gmail.com>
2018-08-09 11:32:18 -0500

    agregue

---
+++ src/pkg15460139/mtc1/MTC1.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pkg15460139.mtc1;
+
+/**
+ *
+ * @author Francisco
+ */
+public class MTC1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+         Principal objeto=new Principal();
+        objeto.show();
+    }
+    
+}
